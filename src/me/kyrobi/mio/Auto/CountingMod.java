a8f6349159ea81cf13fc50d5b48ccd6001ef1a75
Kyrobi <lightling123jr@gmail.com>
2021-12-05 05:05:58 -0600

    Finished the auto counting moderation

--- src/me/kyrobi/mio/Auto/CountingMod.java
+++ src/me/kyrobi/mio/Auto/CountingMod.java
@@ -3,10 +3,8 @@ package me.kyrobi.mio.Auto;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.requests.RestAction;
 
 import java.util.List;
-import java.util.concurrent.atomic.AtomicReference;
 
 public class CountingMod extends ListenerAdapter {
 
@@ -14,15 +12,12 @@ public class CountingMod extends ListenerAdapter {
     public void onGuildMessageReceived(GuildMessageReceivedEvent e){
         if(e.getChannel().getIdLong() == 799121727250235392L){
             System.out.println("A message was sent in the counting channel");
-            System.out.println("Sent: " + e.getMessage().getContentRaw());
 
-            //Gets the last two message and store them to the list
-            List<Message> messages = e.getChannel().getHistory().retrievePast(2).complete();
+            List<Message> messages = e.getChannel().getHistory().retrievePast(2).complete(); //Gets the last two message and store them to the list
 
-            //Access the "2nd" element in the list because the first element is the message a user just sent
-            String prevMessage = messages.get(1).getContentRaw();
             int sent;
             int previous;
+            int expectedValue;
 
             //Try to convert the message to an int to do math
             try{
@@ -30,13 +25,18 @@ public class CountingMod extends ListenerAdapter {
                 previous = Integer.parseInt(messages.get(1).getContentRaw());
             }
 
-            catch(NumberFormatException e){
-
+            catch(NumberFormatException error){
+                System.out.println("Deleted " + e.getMessage().getContentRaw() + " from counting");
+                e.getMessage().delete().queue(); //Assume that if it can't convert, the message isn't right because it's not an int in the first place
+                return;
             }
 
-//            if(e.getMessage().getContentRaw().equals("Haha"){
-//
-//            }
+            expectedValue = previous + 1;
+            if(expectedValue != sent){
+                System.out.println("Deleted " + e.getMessage().getContentRaw() + " from counting");
+                System.out.println("Previous: " + previous + ". Expecting: " + expectedValue);
+                e.getMessage().delete().queue();
+            }
         }
     }
 }
